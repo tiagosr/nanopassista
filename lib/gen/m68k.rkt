10209ec25c3e673aca0649795fb13046a42a15e3
Tiago Rezende <tiagosr@gmail.com>
2015-03-23 23:32:20 -0300

    more and more work on the 68k backend

--- lib/gen/m68k.rkt
+++ lib/gen/m68k.rkt
@@ -40,8 +40,8 @@
 (define temp2-addr  'a2)
 (define temp3-addr  'a2)
 (define jump-target 'a4)
-(define closure-ptr 'a5)
-(define frame-ptr   'a6)
+(define closure-addr 'a5)
+(define frame-addr   'a6)
 
 (define (encode obj)
   (cond
@@ -93,10 +93,16 @@
                                 (cg body (* (+ (length formals) 1) word-size) accum 'return 'ignored)
                                 (cg-code))])))))
 
+(define (varref->address exp)
+  (match exp
+    [`(bound ,n ,name) `(,(* (+ n 1) word-size) ,frame-addr)]
+    [`(free  ,n ,name) `(,(* (+ n 2) word-size) ,closure-addr)]))
+
 ; generates the assembly for each given form
 (define (cg exp frame-size dd cd next-label)
   (match exp
-    ;[`(bound ,n ,name) (cg-load-branch 
+    [`(bound ,n ,name) (cg-load-branch `(,(* (+ n 1) word-size) ,frame-addr) dd cd next-label)]
+    [`(free  ,n ,name) (cg-load-branch `(,(* (+ n 1) word-size) ,closure-addr) dd cd next-label)]
     [`(quote ,obj)  (cg-set-branch obj dd cd next-label)]
     [`(if ,t ,c ,a) (let ([true-label (gen-label "iftrue")]
                           [false-label (gen-label "iffalse")])
@@ -106,8 +112,69 @@
                        (cg c frame-size dd cd false-label)
                        `(label ,false-label)
                        (cg a frame-size dd cd next-label)))]
-    
-    ))
+    [`(build-closure ,code . ,fvars) (if (eq? dd 'effect)
+                                         (error "error in build-closure: not implemented")
+                                         (let ([code-label (gen-label "code")])
+                                           (set! todo (cons (list code-label code) todo))
+                                           (instructions
+                                            `(comment "build-closure")
+                                            (cg-fix-allocate (+ (length fvars) 2) accum-addr (cg-framesize frame-size) '())
+                                            `(move.l ,(header (length fvars) closure-tag) (0 ,accum-addr))
+                                            `(move.l (imm ,code-label) (,(* 1 word-size) ,accum-addr))
+                                            (let loop ([ls fvars]
+                                                       [pos 2])
+                                              (if (null? ls)
+                                                  (instructions)
+                                                  (instructions
+                                                   `(move.l ,(varref->address (car ls)) ,temp1)
+                                                   `(move.l ,temp1 (,(* pos word-size) ,accum-addr))
+                                                   (loop (cdr ls) (+ pos 1)))))
+                                            (cg-type-tag closure-tag accum-addr)
+                                            (cg-store accum-addr dd)
+                                            `(comment "end build-closure")
+                                            (cg-jump cd next-label))))]
+    [else
+     (let ([operator (car exp)]
+           [operands (cdr exp)]
+           [operator-label (gen-label "operator")])
+       (cond
+         [(and (eq? operator 'apply) (eq? cd 'return))
+          (instructions
+           `(comment "apply")
+           (cg-ternary-rands operands frame-size)
+           `(move.l ,temp1-addr (,word-size ,frame-addr))
+           `(move.l ,frame-addr ,temp1-addr)
+           `(addi.l ,temp1-addr ,(* 2 word-size))
+           (let ([loop-label (gen-label "applyloop")]
+                 [break-label (gen-label "applybreak")])
+             (instructions
+              `(label  ,loop-label)
+              `(cmp.l  ,(encode '()) ,temp3-addr)
+              `(beq.l  ,break-label)
+              `(move.l (,(- word-size pair-tag) ,temp3-addr) (0 ,temp1-addr))
+              `(addi.l ,temp1-addr ,word-size)
+              `(move.l (,(- (* 2 word-size) pair-tag) ,temp3-addr) ,temp3-addr)
+              `(jmp ,loop-label)
+              `(label ,break-label)))
+           `(sub.l ,frame-addr ,temp1-addr)
+           `(subi.l ,temp1-addr ,word-size)
+           `(asr.l 2 ,temp1-addr)
+           `(move.l ,temp2-addr ,accum)
+           (cg-jump-closure)
+           `(comment "end apply"))]
+         [(symbol? operator)
+          (cg-inline exp operator operands frame-size dd cd next-label)]
+         [(eq? cd 'return)
+          (instructions
+           (cg-rands operands frame-size)
+           (cg operator (+ frame-size (* (length operands) word-size)) accum operator-label operator-label)
+           `(label ,operator-label)
+           (cg-shuffle frame-size (length operands))
+           `(move.l ,(length operands) ,temp1)
+           (cg-jump-closure))]
+         [else
+          (error "error in else: not implemented")]))]))
+
 
 ; generates the assembly prologue for a given function,
 ; dealing with fixed or variable arguments
@@ -132,15 +199,15 @@
     (if (zero? num)
         (instructions)
         (instructions
-         `(move.l (,frame-ptr ,top) ,temp1)
-         `(move.l ,temp1 (,frame-ptr ,bottom))
+         `(move.l (,frame-addr ,top) ,temp1)
+         `(move.l ,temp1 (,frame-addr ,bottom))
          (loop (+ top word-size) (+ bottom word-size) (- num 1))))))
 
 ;; assemble the different variations of jumps
 (define (cg-jump label next-label)
   (if (eq? label 'return)
       (instructions
-       `(bra.l (,frame-ptr 0)))
+       `(bra.l (,frame-addr 0)))
       (if (eq? label next-label)
           (instructions) ;; nothing (label is the same as the next instruction)
           (instructions `(bra.l ,label)))))
@@ -148,9 +215,9 @@
 ;; assemble a jump that sets up a closure
 (define (cg-jump-closure)
   (instructions
-   `(move.l ,accum ,closure-ptr)
-   `(sub.l  ,closure-tag ,closure-ptr)
-   `(move.l (,closure-ptr ,(* 1 word-size)) ,jump-target)
+   `(move.l ,accum ,closure-addr)
+   `(sub.l  ,closure-tag ,closure-addr)
+   `(move.l (,closure-addr ,(* 1 word-size)) ,jump-target)
    `(jmp    ,jump-target)))
 
 ;; inline translation of builtin operators
@@ -187,8 +254,67 @@
                       `(move.l ,(header 1 symbol-tag) ,accum-addr)
                       `(move.l ,temp1 (,word-size ,accum-addr))
                       (cg-type-tag symbol-tag 'accum-addr)))]
+    [(string)
+     (cg-true-inline cg-rands rands frame-size dd cd next-label
+                     (instructions
+                      `(comment "string")
+                      (cg-fix-allocate
+                       (+ (quotient (+ (length rands) (- word-size 1)) word-size) 1)
+                       accum-addr
+                       (cg-framesize (+ frame-size (* (length rands) word-size)))
+                       '())
+                      `(move.l ,(header (length rands) string-tag) (0 ,accum-addr))
+                      (let loop ([fpos frame-size]
+                                 [spos word-size]
+                                 [num (length rands)])
+                        (if (zero? num)
+                            (instructions)
+                            (instructions
+                             `(move.l (,fpos ,frame-addr) ,temp1)
+                             `(swap ,temp1) ;; swap low and high byte
+                             `(move.b ,temp1 (,spos ,accum-addr))
+                             (loop (+ fpos word-size) (+ spos 1) (- num 1)))))
+                      (cg-type-tag string-tag accum-addr)
+                      `(comment "end string")))]
+    [(%string?)
+     (cg-type-test exp string-tag mask rands frame-size dd cd next-label)]
+    [(%make-vector)
+     (cg-make-vector rands frame-size dd cd next-label vector-tag)]
+                             
     ;;;;;;; TODO other primitives
     ))
+
+(define (cg-make-vector rands frame-size dd cd next-label tag)
+  (cg-true-inline cg-unary-rand rands frame-size dd cd next-label
+                  (instructions
+                   `(asr.l ,tag-len ,temp1)
+                   `(move.l ,temp1 ,temp2)
+                   (if (= tag vector-tag)
+                       `(asl.l 2 ,temp2)
+                       (instructions))
+                   `(move.l ,temp2 ,temp3)
+                   `(add.l ,(+ word-size mask) ,temp2)
+                   `(and.l ,(bitwise-not mask) ,temp2)
+                   (cg-allocate temp2 accum-addr (cg-framesize frame-size) '())
+                   `(asl.l ,attr-len ,temp1)
+                   `(ori.l ,(arithmetic-shift tag 1) ,temp1)
+                   `(move.l ,temp1 (0 ,accum-addr))
+                   (if (= tag vector-tag)
+                       (let ([loop-label (gen-label "fillloop")]
+                             [break-label (gen-label "fillbreak")])
+                         (instructions
+                          `(add.l accum-ptr ,temp3)
+                          `(move.l accum-ptr ,temp1-addr)
+                          `(add.l ,word-size ,temp1-addr)
+                          `(label ,loop-label)
+                          `(cmp.l ,temp2 ,temp3)
+                          `(ble.s ,break-label)
+                          `(move.l 0 (0 ,temp1-addr))
+                          `(add.l ,word-size ,temp1-addr)
+                          `(bra.s ,loop-label)
+                          `(label ,break-label)))
+                       (instructions))
+                   (cg-type-tag tag accum-addr))))
 
 (define (cg-branch true-label false-label next-label jump-if-true jump-if-false)
   (instructions
@@ -210,6 +336,15 @@
      (if (eq? src dest)
          (instructions)
          `(move.l ,src dest))]))
+
+(define (cg-rands operands frame-size)
+  (if (null? operands)
+      (instructions)
+      (let ([operand-label (gen-label "operand")])
+        (instructions
+         (cg (car operands) frame-size `(,frame-size ,frame-addr) operand-label operand-label)
+         `(label ,operand-label)
+         (cg-rands (cdr operands) (+ frame-size word-size))))))
 
 (define (cg-effect-rands ls fs)
   (if (null? ls)
@@ -260,12 +395,31 @@
     (let ([op0-label (gen-label "binary0")]
           [op1-label (gen-label "binary1")])
       (instructions
-       (cg op0 frame-size `(,frame-size ,frame-ptr) op0-label op0-label)
+       (cg op0 frame-size `(,frame-size ,frame-addr) op0-label op0-label)
        `(label ,op0-label)
        (cg op1 (+ frame-size (* 1 word-size)) accum op1-label op1-label)
        `(label ,op1-label)
        `(move.l ,accum ,temp2)
-       `(move.l (,frame-size ,frame-ptr) ,temp1)))))
+       `(move.l (,frame-size ,frame-addr) ,temp1)))))
+
+;; assemble code for ternary operators
+(define (cg-ternary-rands operands frame-size)
+  (let ([r0 (car operands)]
+        [r1 (cadr operands)]
+        [r2 (caddr operands)])
+    (let ([r0-label (gen-label "ternary0")]
+          [r1-label (gen-label "ternary1")]
+          [r2-label (gen-label "ternary2")])
+      (instructions
+       (cg r0 frame-size `(,frame-size ,frame-addr) r0-label r0-label)
+       `(label ,r0-label)
+       (cg r1 (+ frame-size (* 1 word-size)) `(,(+ frame-size (* 1 word-size)) ,frame-addr) r1-label r1-label)
+       `(label ,r1-label)
+       (cg r2 (+ frame-size (* 2 word-size)) accum-addr r2-label r2-label)
+       `(label ,r2-label)
+       `(move.l ,accum-addr ,temp3-addr)
+       `(move.l (,(+ frame-size (* 1 word-size)) ,frame-addr) ,temp2-addr)
+       `(move.l (,frame-size ,frame-addr) ,temp1-addr)))))
 
 (define (cg-ref-inline operator operands frame-size dd cd next-label code)
   (if (eq? dd 'effect)
@@ -315,7 +469,7 @@
       (cg `(if ,exp '#t '#f) frame-size dd cd next-label)))
 
 (define (cg-framesize fs)
-  `(move.l ,fs (,frame-ptr ,(- word-size))))
+  `(move.l ,fs (,frame-addr ,(- word-size))))
 
 (define (cg-allocate sizecode target frameinfocode usedregs)
   (let ([allocate
@@ -333,12 +487,12 @@
       `(comment "gc")
       `(sub.l ,sizecode ,accum)
       frameinfocode
-      `(movem.l [,temp3 ,temp2 ,temp1 ,accum ,closure-ptr ,frame-ptr] (a7-))
+      `(movem.l [,temp3 ,temp2 ,temp1 ,accum ,closure-addr ,frame-addr] (a7-))
       ;(movem.l ,(encode-regs usedregs))
       `(move.l ,accum (a7+))
       `(call 'gc-collect)
       `(add.l ,(* 7 word-size) a7)
-      `(movem.l [,temp3 ,temp2 ,temp1 ,accum ,closure-ptr ,frame-ptr] (+a7))
+      `(movem.l [,temp3 ,temp2 ,temp1 ,accum ,closure-addr ,frame-addr] (+a7))
       `(move.l _gc_free ,accum-addr)
       (allocate faultcode)
       `(comment "end gc")))))
