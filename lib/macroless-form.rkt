fa35c05f8678bb1971ea4378bebe35802ef26fa9
Tiago Rezende <tiagosr@gmail.com>
2015-03-26 03:08:51 -0300

    removed weird r4rs stuff that was lost in the way

--- lib/macroless-form.rkt
+++ lib/macroless-form.rkt
@@ -167,54 +167,7 @@
     ;TODO fill this up
     ))
 
-#|
- | expands macros and applies the base library
- |#
 
-(define (macroless-form exp)
-  (if (begin-exp? exp)
-      exp
-      `(begin ,@(remove define-macro-exp?
-                        (expand-top-level (cdr exp))))))
-
-(define (prepend-library exp)
-  (if (begin-exp? exp)
-      `(begin ,@library ,@(cdr exp))
-      `(begin ,@library ,exp)))
-
-(define (expand-top-level exps)
-  (let ([env (make-base-namespace)])
-    (map (lambda (e)
-           (match e
-             [`(define-macro _ _)
-              (eval e env)
-              e]
-             [`(define-syntax ,id ,transformer)
-              (macroexpand env)]
-             [else (my-expand e env)])) exps)))
-
-(define (my-expand exp env)
-  (if (pair? exp)
-      (match exp
-        [`(define (,var . ,formals) ,e) `(define ,var (lambda ,formals ,(my-expand e env)))]
-        [`(define ,var ,e) `(define ,var ,(my-expand e env))]
-        [`(quote ,obj) `(quote ,obj)]
-        [`(begin . ,exps) `(begin ,@(map (lambda (e) (my-expand e env)) exps))]
-        [`(if ,t ,c ,a) `(if ,(my-expand t env) ,(my-expand c env) ,(my-expand a env))]
-        [`(set! ,v ,e) `(set! ,v ,(my-expand e env))]
-        [`(lambda ,formals . ,bodies) `(lambda ,formals ,@(map (lambda (e) (my-expand e env)) bodies))]
-        [else (let ([r (macroexpand exp)])
-                (if (equal? exp r)
-                    (map (lambda (e) (my-expand e env)) r)
-                    (my-expand r env)))])
-      exp))
-
-(define (my-define-macro def . body)
-  (if (pair? def)
-      `(my-define-macro ,(car def) (lambda ,(cdr def) ,@body))
-      `(hashtable/put! all-macros ',def ,@body)))
-(define (macroexpand-1 exp)
-  (void))
 
 
 (define (set-global-syntax name x env)
